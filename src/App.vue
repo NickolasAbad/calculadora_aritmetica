7f61f5b325629da2a9caefd4f15d61afa272a39b
Nickolas Abad <123907645+NickolasAbad@users.noreply.github.com>
2023-06-23 01:28:19 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Header from './components/Header.vue'
+    import Calculator from './components/Calculator.vue'
+    import Result from './components/Result.vue'
+    import { getOperacao, estado } from './scripts/Scripts.vue'
+</script>
+
+<template>
+  <Header />
+  <Calculator :estado="estado"/>
+  <Result :estado="estado" :operacao="getOperacao()"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        <div class="header_content container mt-4 d-flex justify-content-center">
+        <h1>Calculadora Aritmética</h1>
+        </div>
+    </header>
+</template>
